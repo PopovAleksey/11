7ea8b03f5446fdf7991d39017659454b2235a284
Aleksey Popov <popov.scar@gmail.com>
2022-04-25 00:03:43 +0300

    Fix

--- app/Containers/Dashboard/Configuration/Tasks/GetAllMenuConfigurationTask.php
+++ app/Containers/Dashboard/Configuration/Tasks/GetAllMenuConfigurationTask.php
@@ -26,35 +26,46 @@ class GetAllMenuConfigurationTask extends Task implements GetAllMenuConfiguratio
 
     public function run(): Collection
     {
-        $pageList      = $this->pageRepository->all()->collect()
+        $menuList   = $this->configurationMenuRepository
+            ->all()
+            ->collect()
+            ->keyBy('content_id');
+        $pageIds    = $this->pageRepository
+            ->all()
+            ->collect()
             ->reject(fn(PageInterface $page) => $page->parent_page_id !== null || $page->active === false)
-            ->keyBy('id');
-        $menuList      = $this->configurationMenuRepository->all()->collect()->keyBy('content_id');
-        $contentList   = $this->contentRepository
-            ->findWhereIn('page_id', $pageList->keys()->toArray())->keyBy('id');
-        $contentValues = $this->contentValueRepository
+            ->keyBy('id')
+            ->keys()
+            ->toArray();
+        $contentIds = $this->contentRepository
+            ->findWhereIn('page_id', $pageIds)
+            ->keyBy('id')
+            ->keys()
+            ->toArray();
+
+
+        return $this->contentValueRepository
             ->orderBy('id', 'asc')
-            ->findWhereIn('content_id', $contentList->keys()->toArray())
+            ->findWhereIn('content_id', $contentIds)
             ->filter(fn(ContentValueInterface $value) => $value->language_id === 1)
-            ->groupBy('content_id');
+            ->groupBy('content_id')
+            ->map(static function (Collection $content) use ($menuList) {
+                /**
+                 * @var ContentValueInterface $contentValue
+                 */
+                $contentValue = $content->first();
+                /**
+                 * @var ConfigurationMenuInterface|null $menuItem
+                 */
+                $menuItem = $menuList->get($contentValue->content_id);
 
-        return $contentValues->map(static function (Collection $content) use ($menuList) {
-            /**
-             * @var ContentValueInterface $contentValue
-             */
-            $contentValue = $content->first();
-            /**
-             * @var ConfigurationMenuInterface|null $menuItem
-             */
-            $menuItem = $menuList->get($contentValue->content_id);
-
-            return (new MenuDto())
-                ->setId($menuItem?->id)
-                ->setName($contentValue->value)
-                ->setInMenu(!is_null($menuItem))
-                ->setContentId($contentValue->content_id)
-                ->setCreateAt($menuItem?->created_at)
-                ->setUpdateAt($menuItem?->updated_at);
-        });
+                return (new MenuDto())
+                    ->setId($menuItem?->id)
+                    ->setName($contentValue->value)
+                    ->setInMenu(!is_null($menuItem))
+                    ->setContentId($contentValue->content_id)
+                    ->setCreateAt($menuItem?->created_at)
+                    ->setUpdateAt($menuItem?->updated_at);
+            });
     }
 }
