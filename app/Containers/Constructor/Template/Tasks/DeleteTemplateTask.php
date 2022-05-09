0deaa07c6cf02fdcbcf3eaa7f1a0d787c7d4663e
Aleksey Popov <popov.scar@gmail.com>
2022-05-09 14:45:53 +0300

    Template: Delete Template files

--- app/Containers/Constructor/Template/Tasks/DeleteTemplateTask.php
+++ app/Containers/Constructor/Template/Tasks/DeleteTemplateTask.php
@@ -2,10 +2,12 @@
 
 namespace App\Containers\Constructor\Template\Tasks;
 
+use App\Ship\Exceptions\DeleteResourceFailedException;
 use App\Ship\Parents\Models\TemplateInterface;
 use App\Ship\Parents\Repositories\TemplateRepositoryInterface;
 use App\Ship\Parents\Repositories\ThemeRepositoryInterface;
 use App\Ship\Parents\Tasks\Task;
+use Exception;
 use Storage;
 
 class DeleteTemplateTask extends Task implements DeleteTemplateTaskInterface
@@ -24,30 +26,39 @@ class DeleteTemplateTask extends Task implements DeleteTemplateTaskInterface
      */
     public function run(int $id): ?bool
     {
-        #try {
-        /**
-         * @var \App\Ship\Parents\Models\TemplateInterface $template
-         */
-        $template = $this->templateRepository->find($id);
-        $theme    = $this->themeRepository->find($template->theme_id);
+        try {
+            /**
+             * @var \App\Ship\Parents\Models\TemplateInterface $template
+             */
+            $template = $this->templateRepository->find($id);
+            $theme    = $this->themeRepository->find($template->theme_id);
 
-        $this->templateRepository->delete($template->id);
+            $this->templateRepository->delete($template->id);
 
-        $folder = match ($template->type) {
-            TemplateInterface::CSS_TYPE => config('constructor-template.folderName.css'),
-            TemplateInterface::JS_TYPE => config('constructor-template.folderName.js'),
-            default => config('constructor-template.folderName.view'),
-        };
+            [$folder, $type] = match ($template->type) {
+                TemplateInterface::CSS_TYPE => [
+                    config('constructor-template.folderName.css'),
+                    config('constructor-template.fileType.css'),
+                ],
+                TemplateInterface::JS_TYPE => [
+                    config('constructor-template.folderName.js'),
+                    config('constructor-template.fileType.js'),
+                ],
+                default => [
+                    config('constructor-template.folderName.view'),
+                    config('constructor-template.fileType.view'),
+                ],
+            };
 
-        $path = implode('/', [$theme->directory, $folder]);
+            $path = implode('/', [$theme->directory, $folder]);
 
-        Storage::disk('template')->delete($path . '/' . $template->common_filepath);
-        Storage::disk('template')->delete($path . '/' . $template->element_filepath);
-        Storage::disk('template')->delete($path . '/' . $template->preview_filepath);
+            Storage::disk('template')->delete($path . '/' . $template->common_filepath . $type);
+            Storage::disk('template')->delete($path . '/' . $template->element_filepath . $type);
+            Storage::disk('template')->delete($path . '/' . $template->preview_filepath . $type);
 
-        return true;
-        /*} catch (Exception) {
+            return true;
+        } catch (Exception) {
             throw new DeleteResourceFailedException();
-        }*/
+        }
     }
 }
