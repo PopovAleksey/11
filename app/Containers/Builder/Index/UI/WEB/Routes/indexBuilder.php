fab64833cd8b08dba8fde9effda628104524aa67
Aleksey Popov <popov.scar@gmail.com>
2022-05-17 13:57:53 +0300

    Builder: Fix including CSS and JS files

--- app/Containers/Builder/Index/UI/WEB/Routes/indexBuilder.php
+++ app/Containers/Builder/Index/UI/WEB/Routes/indexBuilder.php
@@ -3,14 +3,14 @@
 use App\Containers\Builder\Index\UI\WEB\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{themeName}/{type}/{file}', [Controller::class, 'file'])
+Route::get('/{themeName}/{type}/{file}', [Controller::class, 'cssFile'])
     ->where(['type' => '(css|js)'])
     ->name('builder_js_page');
 
 Route::get('/{language?}/{seoLink?}', [Controller::class, 'index'])
     ->where([
         'language' => '[A-Za-z]{2}',
-        'seoLink' => '[A-Za-z0-9\-_]+'
+        'seoLink'  => '[A-Za-z0-9\-_]+',
     ])
     ->name('builder_index_page');
 
