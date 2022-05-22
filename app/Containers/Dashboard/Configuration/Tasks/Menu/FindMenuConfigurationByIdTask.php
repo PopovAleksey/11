00d0f7e342e3210b5c9084182dec3d7319e9fae8
Aleksey Popov <popov.scar@gmail.com>
2022-05-22 23:45:54 +0300

    Refactoring

--- app/Containers/Dashboard/Configuration/Tasks/Menu/FindMenuConfigurationByIdTask.php
+++ app/Containers/Dashboard/Configuration/Tasks/Menu/FindMenuConfigurationByIdTask.php
@@ -4,24 +4,14 @@ namespace App\Containers\Dashboard\Configuration\Tasks\Menu;
 
 use App\Ship\Parents\Dto\ConfigurationMenuDto;
 use App\Ship\Parents\Dto\ConfigurationMenuItemDto;
-use App\Ship\Parents\Models\ConfigurationMenuInterface;
 use App\Ship\Parents\Models\ConfigurationMenuItemInterface;
-use App\Ship\Parents\Models\ContentValueInterface;
-use App\Ship\Parents\Models\PageInterface;
 use App\Ship\Parents\Repositories\ConfigurationMenuRepositoryInterface;
-use App\Ship\Parents\Repositories\ContentRepositoryInterface;
-use App\Ship\Parents\Repositories\ContentValueRepositoryInterface;
-use App\Ship\Parents\Repositories\PageRepositoryInterface;
 use App\Ship\Parents\Tasks\Task;
-use Illuminate\Support\Collection;
 
 class FindMenuConfigurationByIdTask extends Task implements FindMenuConfigurationByIdTaskInterface
 {
     public function __construct(
-        private PageRepositoryInterface              $pageRepository,
         private ConfigurationMenuRepositoryInterface $configurationMenuRepository,
-        private ContentRepositoryInterface           $contentRepository,
-        private ContentValueRepositoryInterface      $contentValueRepository
     )
     {
     }
@@ -52,47 +42,5 @@ class FindMenuConfigurationByIdTask extends Task implements FindMenuConfiguratio
             ->setItems($items)
             ->setCreateAt($menu->created_at)
             ->setUpdateAt($menu->updated_at);
-
-        /*$menuList   = $this->configurationMenuRepository
-            ->all()
-            ->collect()
-            ->keyBy('content_id');
-        $pageIds    = $this->pageRepository
-            ->all()
-            ->collect()
-            ->reject(fn(PageInterface $page) => $page->parent_page_id !== null || $page->active === false)
-            ->keyBy('id')
-            ->keys()
-            ->toArray();
-        $contentIds = $this->contentRepository
-            ->findWhereIn('page_id', $pageIds)
-            ->keyBy('id')
-            ->keys()
-            ->toArray();
-
-
-        return $this->contentValueRepository
-            ->orderBy('id')
-            ->findWhereIn('content_id', $contentIds)
-            ->filter(fn(ContentValueInterface $value) => $value->language_id === 1)
-            ->groupBy('content_id')
-            ->map(static function (Collection $content) use ($menuList) {
-                /**
-                 * @var ContentValueInterface $contentValue
-                 */
-                $contentValue = $content->first();
-                /**
-                 * @var ConfigurationMenuInterface|null $menuItem
-                 * /
-                $menuItem = $menuList->get($contentValue->content_id);
-
-                return (new ConfigurationMenuDto())
-                    ->setId($menuItem?->id)
-                    ->setName($contentValue->value)
-                    ->setInMenu(!is_null($menuItem))
-                    ->setContentId($contentValue->content_id)
-                    ->setCreateAt($menuItem?->created_at)
-                    ->setUpdateAt($menuItem?->updated_at);
-            });*/
     }
 }
