c180107bf0e7dddee08e68dd461a0e1c71589f0d
Oleksii Popov <popov.scar@gmail.com>
2023-11-10 22:44:11 +0200

    Fixed install script and .env.example

--- docker/install.sh
+++ docker/install.sh
@@ -1,1 +1,1 @@
-cp .env.example .env && COMPOSER_PROCESS_TIMEOUT=2000 composer install && php artisan db:seed
+cp .env.example .env && COMPOSER_PROCESS_TIMEOUT=2000 composer install && php artisan key:generate --ansi && php artisan db:seed
